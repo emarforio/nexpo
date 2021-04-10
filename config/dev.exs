63b09b8072e1775026c13fdfa318807a8588f805
Erik Marforio <emarforio@gmail.com>
2021-04-10 11:54:41 +0200

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -51,7 +51,10 @@ config :nexpo, Nexpo.Mailer, adapter: Bamboo.LocalAdapter
 config :nexpo, Nexpo.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "nexpo_dev",
-  pool_size: 10
+  pool_size: 10,
+  username: "nexpo",
+  password: "nexpo",
+  hostname: "db"
 
 config :arc,
   # or Arc.Storage.Local
